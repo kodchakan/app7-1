318e4087e5d85f039686e2cab13999269c02ecea
kodchakan <265345221001-st@rmutsb.ac.th>
2023-05-26 10:08:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 //import FormSearch from './form/form-search';
-import FormBootstrap from './form/form-bootstrap';
+//import FormBootstrap from './form/form-bootstrap';
+import FormValidation from './form/form-validation';
 
 function App() {
   return (
     //<FormSearch />
-    <FormBootstrap />
+    //<FormBootstrap />
+    <FormValidation />
   )
 }
 
